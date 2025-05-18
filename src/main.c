e21048fc01bbb3b012daea1e695c51df21eec35f
Jadilson <jadilsonigor@gmail.com>
2025-05-18 20:45:14 -0300

    :sparkles: menu dinamico de atendimento ao cliente

--- src/main.c
+++ src/main.c
@@ -9,12 +9,64 @@
 void menu()
 {
     printf("\n--- Menu Lista ---\n");
-    printf("1. Ver cardapio\n");
+    printf("1. Atender cliente\n");
     printf("2. Enviar pedido para a cozinha\n");
     printf("3. Mostra pedidos na cozinha\n");
     printf("4. Imprimir lista\n");
     printf("5. Sair\n");
     printf("Escolha uma opcao: ");
+}
+
+void atendimento() {
+    printf("\n--- Opcoes de atendimento ---\n");
+    printf("1. Exibir cardapio\n");
+    printf("2. Enviar pedido para a cozinha\n");
+    printf("3. Exibir pedido\n");
+    printf("4. Sair\n");
+    printf("Escolha uma opcao: ");
+}
+
+void switchCardapio(int op_cardapio, int resposta, int qtd, No** pedido, Prato prato) {
+
+    switch (op_cardapio)
+        {
+        case 1:
+            cardapio_entrada();
+            scanf("%d", &resposta);
+            printf("Qual quantidade?\n");
+            scanf("%d", &qtd);
+            criar_prato(entrada[resposta - 1], qtd, &prato);
+            insercao_no_fim_da_lsl(pedido, prato);
+            printf("Prato inserido no inicio.\n");
+
+            break;
+
+        case 2:
+            cardapio_principal();
+            scanf("%d", &resposta);
+            printf("Qual quantidade?\n");
+            scanf("%d", &qtd);
+            criar_prato(principal[resposta - 1], qtd, &prato);
+            insercao_no_fim_da_lsl(pedido, prato);
+            printf("Prato inserido no inicio.\n");
+
+            break;
+
+        case 3:
+            cardapio_sobremesa();
+            scanf("%d", &resposta);
+            printf("Qual quantidade?\n");
+            scanf("%d", &qtd);
+            criar_prato(sobremesa[resposta - 1], qtd, &prato);
+            insercao_no_fim_da_lsl(pedido, prato);
+            printf("Prato inserido no inicio.\n");
+
+            break;
+
+        default:
+            printf("Não existe a opção desejada!");
+            break;
+        }
 }
 
 int main()
@@ -24,6 +76,7 @@ int main()
     Prato prato;
     int opcao;
     int op_cardapio; // opção dos cardápios
+    int op_atendimento; // opção de atendimento  ao cliente
     int resposta, qtd;
     char buffer[100];
     int pos;
@@ -39,50 +92,46 @@ int main()
         switch (opcao)
         {
         case 1:
-            printf("Qual e o cardapio que voce deseja acessar: 1- Entrada, 2- Principais, 3 - Sobremesa:\n");
-            scanf("%d", &op_cardapio);
-            getchar(); // limpa buffer
+            do {    
+                atendimento();
+                scanf("%d", &op_atendimento);
+                getchar(); // limpa buffer
 
-            switch (op_cardapio)
-            {
-            case 1:
-                cardapio_entrada();
-                scanf("%d", &resposta);
-                printf("Qual quantidade?\n");
-                scanf("%d", &qtd);
-                criar_prato(entrada[resposta - 1], qtd, &prato);
-                insercao_no_fim_da_lsl(&pedido, prato);
-                printf("Prato inserido no inicio.\n");
+                switch (op_atendimento)
+                    {
+                    case 1:
+                        printf("Qual e o cardapio que voce deseja acessar: 1- Entrada, 2- Principais, 3 - Sobremesa:\n");
+                        scanf("%d", &op_atendimento);
+                        switchCardapio(op_atendimento, resposta, qtd, &pedido, prato);
+                        break;
 
-                break;
+                    case 2:
+                        if(pedido == NULL) {
+                            printf("Nao existem pratos inseridos no pedido, insira algum prato para enviar para a cozinha!\n");
+                            break;
+                        }
 
-            case 2:
-                cardapio_principal();
-                scanf("%d", &resposta);
-                printf("Qual quantidade?\n");
-                scanf("%d", &qtd);
-                criar_prato(principal[resposta - 1], qtd, &prato);
-                insercao_no_fim_da_lsl(&pedido, prato);
-                printf("Prato inserido no inicio.\n");
+                        //Copiamos a lista original e enviamos para a fila
+                        No* copia_do_pedido = copiar_lista(pedido);
+                        inserir_fila(&fila, copia_do_pedido);
 
-                break;
+                        //esvaziamos a lista para poder fazer um novo pedido
+                        while (pedido != NULL) {
+                            remocao_no_inicio_da_lsl(&pedido);
+                        }
 
-            case 3:
-                cardapio_sobremesa();
-                scanf("%d", &resposta);
-                printf("Qual quantidade?\n");
-                scanf("%d", &qtd);
-                criar_prato(sobremesa[resposta - 1], qtd, &prato);
-                insercao_no_fim_da_lsl(&pedido, prato);
-                printf("Prato inserido no inicio.\n");
+                        printf("Pedido enviado para a cozinha com sucesso!\n");
+                        break;
 
-                break;
+                    case 3:
+                        imprime_a_lsl(pedido);
+                        break;
 
-            default:
-                printf("Não existe a opção desejada!");
-                break;
-            }
-            break;
+                    default:
+                        printf("Não existe a opção desejada!");
+                        break;
+                    }
+            } while (op_atendimento != 4);
 
         case 2:
     
